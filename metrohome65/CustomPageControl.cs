2d228cd3dd769c75602dc17bcf709c8533ed25b8
SeregaP <SeregaP@Parfenov-mobile.mail.sms-samara.ru>
2011-05-18 07:57:49 +0400

    change folder and unit sctructure

---
+++ metrohome65/CustomPageControl.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MetroHome65.Pages
+{
+    class CustomPageControl : Panel, IPageControl
+    {
+        public virtual void SetScrollPosition(Point Location) { }
+
+        public virtual Point GetScrollPosition() { return new Point(0, 0); }
+
+        public virtual Size GetViewportSize() { return this.Size; }
+        public virtual Size GetExtentSize() {return this.Size; }
+
+        public virtual void ClickAt(Point Location) { }
+
+        public virtual void DblClickAt(Point Location) { }
+
+        public virtual Boolean ShowPopupMenu(Point Location) { return false; }
+
+        public Boolean Active { set { SetActive(value); } }
+
+        protected virtual void SetActive(Boolean value) { }
+
+        public virtual Control GetControl() { return this; }
+
+        public virtual void SetBackColor(Color value) { this.BackColor = value; }
+    }
+}
---
+++ metrohome65/PageControl.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MetroHome65.Pages
+{
+    interface IPageControl 
+    {
+        void SetScrollPosition(Point Location);
+        Point GetScrollPosition();
+
+        Size GetViewportSize();
+        Size GetExtentSize();
+
+        void ClickAt(Point Location);
+        void DblClickAt(Point Location);
+        Boolean ShowPopupMenu(Point Location);
+        
+        Boolean Active { set; }
+
+        void SetBackColor(Color value);
+
+        Control GetControl();
+    }
+}
