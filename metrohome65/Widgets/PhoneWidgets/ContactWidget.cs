deb9a5b180fd7209d11710dc374a5a8cc1f9f729
SeregaP <SeregaP@Parfenov-mobile.mail.sms-samara.ru>
2011-05-19 07:49:43 +0400

    Added ItemId property to Contact widget

--- metrohome65/Widgets/PhoneWidgets/ContactWidget.cs
+++ metrohome65/Widgets/PhoneWidgets/ContactWidget.cs
@@ -8,15 +8,31 @@ namespace MetroHome65.Widgets
 
     public class ContactWidget : BaseWidget
     {
+        [WidgetParameter]
+        public String ItemId = null;
+
         protected override String[] GetMenuItems()
         {
             String[] Items = { "Call", "Send SMS" };
             return Items;
         }
 
-        public override void OnMenuItemClick(String ItemName)
+        Contact FindContact(String ItemIdKey)
         {
-            MessageBox.Show(ItemName);
+            Contact FindedContact = null;
+            OutlookSession mySession = new OutlookSession();
+            
+            ContactCollection collection = mySession.Contacts.Items;
+            foreach (Contact contact in collection)
+            {
+                if (contact.ItemId.GetHashCode().Equals(ItemIdKey))
+                {
+                    FindedContact = contact;
+                    break;
+                }
+            }
+
+            return FindedContact;
         }
 
         public override void Paint(Graphics g, Rectangle Rect)
@@ -24,27 +40,35 @@ namespace MetroHome65.Widgets
             Pen BorderPen = new System.Drawing.Pen(Color.Gray, 2);
             g.DrawRectangle(BorderPen, Rect.Left, Rect.Top, Rect.Width - 2, Rect.Height - 2);
 
-            String ContactName = "";
+            Font captionFont = new System.Drawing.Font("Helvetica", 9, FontStyle.Regular);
+            Brush captionBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
 
-            OutlookSession mySession = new OutlookSession();
-            
-            ContactCollection collection = mySession.Contacts.Items;
-            foreach (Contact contact in collection)
+            Contact contact = FindContact(this.ItemId);
+
+            if (contact == null)
             {
-                ContactName = contact.FileAs;
-                //contact.ItemId 
-                if (contact.Picture != null)
-                    g.DrawImage(contact.Picture,
-                        new Rectangle(Rect.Left + 1, Rect.Top + 1, Rect.Width - 3, Rect.Height - 3),
-                        0, 0, contact.Picture.Width, contact.Picture.Height, GraphicsUnit.Pixel,
-                        new System.Drawing.Imaging.ImageAttributes());
-
-                Font captionFont = new System.Drawing.Font("Helvetica", 9, FontStyle.Regular);
-                Brush captionBrush = new System.Drawing.SolidBrush(System.Drawing.Color.White);
-                g.DrawString(ContactName, captionFont, captionBrush,
-                    Rect.Left + 10, Rect.Bottom - 5 - g.MeasureString(ContactName, captionFont).Height);
+                g.DrawString("Contact not found", captionFont, captionBrush,
+                    Rect.Left + 10, Rect.Top + 10);
+                return;
             }
+
+            String ContactName = contact.FileAs;
+            if (contact.Picture != null)
+                g.DrawImage(contact.Picture,
+                    new Rectangle(Rect.Left + 1, Rect.Top + 1, Rect.Width - 3, Rect.Height - 3),
+                    0, 0, contact.Picture.Width, contact.Picture.Height, GraphicsUnit.Pixel,
+                    new System.Drawing.Imaging.ImageAttributes());
+
+            g.DrawString(ContactName, captionFont, captionBrush,
+                Rect.Left + 10, Rect.Bottom - 5 - g.MeasureString(ContactName, captionFont).Height);
         }
+
+
+        public override void OnMenuItemClick(String ItemName)
+        {
+            MessageBox.Show(ItemName);
+        }
+
     }
 
 }
