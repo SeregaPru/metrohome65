55056e4b4f655c09d562249c0dc678981a256457
SeregaPru <SeregaPru@localhost>
2011-11-03 07:23:47 +0400

    change drawing proc for transparent widgets

--- metrohome65/MetroHome65.Routines/AlphaImage.cs
+++ metrohome65/MetroHome65.Routines/AlphaImage.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Drawing;
 using OpenNETCF.Drawing;
+using System.Runtime.InteropServices;
 
 
 namespace MetroHome65.Routines
@@ -60,31 +61,6 @@ namespace MetroHome65.Routines
         }
 
 
-        /// <summary>
-        /// Paints image as backgroud for rect.
-        /// </summary>
-        /// <param name="g"></param>
-        /// <param name="Rect"></param>
-        public void PaintBackground(Graphics g, Rectangle Rect)
-        {
-            // if button image is set, draw button image
-            if (_img != null)
-            {
-                try
-                {
-                    IntPtr hdc = g.GetHdc();
-                    OpenNETCF.Drawing.Imaging.RECT ImgRect = OpenNETCF.Drawing.Imaging.RECT.FromXYWH(Rect.Left, Rect.Top, Rect.Width, Rect.Height);
-                    _img.Draw(hdc, ImgRect, null);
-                    g.ReleaseHdc(hdc);
-                    return;
-                }
-                catch (Exception e)
-                {
-                    //!! write to log  (e.StackTrace, "PaintBackground")
-                }
-            }
-        }
-
         public void PaintIcon(Graphics g, Rectangle Rect)
         {
             // draw icon from external image file
@@ -110,5 +86,135 @@ namespace MetroHome65.Routines
             }
         }
 
+
+        /// <summary>
+        /// Paints image as backgroud for rect.
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="Rect"></param>
+        public void PaintBackground(Graphics g, Rectangle Rect)
+        {
+            // if button image is set, draw button image
+            if (_img != null)
+            {
+                try
+                {
+                    IntPtr hdc = g.GetHdc();
+                    OpenNETCF.Drawing.Imaging.RECT ImgRect = OpenNETCF.Drawing.Imaging.RECT.FromXYWH(Rect.Left, Rect.Top, Rect.Width, Rect.Height);
+                    _img.Draw(hdc, ImgRect, null);
+                    g.ReleaseHdc(hdc);
+                    return;
+                }
+                catch (Exception e)
+                {
+                    //!! write to log  (e.StackTrace, "PaintBackground")
+                }
+            }
+        }
+
+        public void PaintBackgroundAlpha(Graphics g, Rectangle Rect, byte Alpha)
+        {
+            //!! see _factory.CreateBitmapFromImage
+
+            Bitmap buffer = new Bitmap(Rect.Width, Rect.Height, System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            Graphics buffergx = Graphics.FromImage(buffer);
+            IntPtr hdcBuffer;
+
+            Rectangle BufferRect = new Rectangle(0, 0, Rect.Width, Rect.Height);
+            if (_img != null)
+            {
+                hdcBuffer = buffergx.GetHdc();
+                try
+                {
+                    OpenNETCF.Drawing.Imaging.RECT ImgRect = OpenNETCF.Drawing.Imaging.RECT.FromXYWH(
+                        BufferRect.Left, BufferRect.Top, BufferRect.Width, BufferRect.Height);
+                    _img.Draw(hdcBuffer, ImgRect, null);
+                    buffergx.ReleaseHdc(hdcBuffer);
+                }
+                catch (Exception e) { }
+
+                IntPtr hdcDst = g.GetHdc();
+                hdcBuffer = buffergx.GetHdc();
+                BlendFunction blendFunction = new BlendFunction();
+                blendFunction.BlendOp = (byte)BlendOperation.AC_SRC_OVER;   // Only supported blend operation
+                blendFunction.BlendFlags = (byte)BlendFlags.Zero;           // Documentation says put 0 here
+                blendFunction.SourceConstantAlpha = Alpha;                 // Constant alpha factor
+                blendFunction.AlphaFormat = (byte)AlphaFormat.AC_SRC_ALPHA;
+
+                DrawingAPI.AlphaBlend(hdcDst, Rect.Left, Rect.Top, Rect.Width, Rect.Height, hdcBuffer,
+                    0, 0, Rect.Width, Rect.Height, blendFunction);
+
+                g.ReleaseHdc(hdcDst);          // Required cleanup to GetHdc()
+                buffergx.ReleaseHdc(hdcBuffer);       // Required cleanup to GetHdc()
+            }
+
+        }
+
+
+        public static void DrawAlphaImage(Graphics g, Image Img, Rectangle Rect, byte Alpha)
+        {
+            IntPtr hdcDst = g.GetHdc();
+            Graphics gSrc = Graphics.FromImage(Img);
+            IntPtr hdcSrc = gSrc.GetHdc();
+
+            BlendFunction blendFunction = new BlendFunction();
+            blendFunction.BlendOp = (byte)BlendOperation.AC_SRC_OVER;   // Only supported blend operation
+            blendFunction.BlendFlags = (byte)BlendFlags.Zero;           // Documentation says put 0 here
+            blendFunction.SourceConstantAlpha = Alpha;                 // Constant alpha factor
+            blendFunction.AlphaFormat = (byte)0; // AlphaFormat.AC_SRC_ALPHA;
+            //!!blendFunction.AlphaFormat = (byte)AlphaFormat.AC_SRC_ALPHA;
+
+            DrawingAPI.AlphaBlend(hdcDst, Rect.Left, Rect.Top, Rect.Width, Rect.Height, hdcSrc,
+                0, 0, Img.Width, Img.Height, blendFunction);
+
+            g.ReleaseHdc(hdcDst);          // Required cleanup to GetHdc()
+            gSrc.ReleaseHdc(hdcSrc);       // Required cleanup to GetHdc()
+        }
     }
+
+
+    // These structures, enumerations and p/invoke signatures come from
+    // wingdi.h in the Windows Mobile 5.0 Pocket PC SDK
+
+    public struct BlendFunction
+    {
+        public byte BlendOp;
+        public byte BlendFlags;
+        public byte SourceConstantAlpha;
+        public byte AlphaFormat;
+    }
+
+    public enum BlendOperation : byte
+    {
+        AC_SRC_OVER = 0x00
+    }
+
+    public enum BlendFlags : byte
+    {
+        Zero = 0x00
+    }
+
+    public enum SourceConstantAlpha : byte
+    {
+        Transparent = 0x00,
+        Opaque = 0xFF
+    }
+
+    public enum AlphaFormat : byte
+    {
+        AC_SRC_ALPHA = 0x01
+    }
+
+    public class DrawingAPI
+    {
+        [DllImport("coredll.dll")]
+        extern public static bool AlphaBlend(IntPtr hdcDest, Int32 nXDest, Int32 nYDest, Int32 nWidthDst, Int32 nHeightDst, IntPtr hdcSrc, Int32 nXSrc, Int32 nYSrc, Int32 nWidthSrc, Int32 nHeightSrc, BlendFunction blendFunction);
+
+        [DllImport("coredll.dll")]
+        extern public static bool BitBlt(IntPtr hdcDest, Int32 nXDest, Int32 nYDest, Int32 nWidth, Int32 nHeight, IntPtr hdcSrc, Int32 nXSrc, Int32 nYSrc, UInt32 dwRop);
+
+        public const UInt32 SRCCOPY = 0x00CC0020;
+    }
+
+
 }
--- metrohome65/WidgetWrapper.cs
+++ metrohome65/WidgetWrapper.cs
@@ -47,18 +47,13 @@ namespace MetroHome65.Pages
         private bool _Moving = false;
 
         // double buffer
-        private Bitmap _DoubleBuffer = null;
-        private Graphics _graphics = null;
+        private Bitmap _BufferImg = null;
+        private Graphics _BufferGr = null;
         private bool _needRepaint = true;
 
 
-        //[XmlIgnore]
-        //public WidgetGrid WidgetGrid = null;
 
-
-        public WidgetWrapper()
-        {
-        }
+        public WidgetWrapper()  { }
 
         ~WidgetWrapper()
         {
@@ -244,52 +239,70 @@ namespace MetroHome65.Pages
 
         private void ClearBuffer()
         {
-            if (_graphics != null)
+            if (_BufferGr != null)
             {
-                _graphics.Dispose();
-                _graphics = null;
+                _BufferGr.Dispose();
+                _BufferGr = null;
             }
-            if (_DoubleBuffer != null)
+            if (_BufferImg != null)
             {
-                _DoubleBuffer.Dispose();
-                _DoubleBuffer = null;
+                _BufferImg.Dispose();
+                _BufferImg = null;
             }
         }
+
+        byte _Alpha = 200;
 
         private void PaintBuffer()
         {
             ClearBuffer();
 
-            _DoubleBuffer = new Bitmap(ScreenRect.Width, ScreenRect.Height);
-            _graphics = Graphics.FromImage(_DoubleBuffer);
+            // create buffer bitmap with alpha channel
+            _BufferImg = new Bitmap(ScreenRect.Width, ScreenRect.Height, 
+                System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            _BufferGr = Graphics.FromImage(_BufferImg);
+            _BufferGr.Clear(Color.Transparent);
 
-            Rectangle Rect = new Rectangle(0, 0, _DoubleBuffer.Width, _DoubleBuffer.Height);
+            Rectangle Rect = new Rectangle(0, 0, ScreenRect.Width, ScreenRect.Height);
 
+            // create temp bitmap with alpha channel
+            Bitmap _TempImg = new Bitmap(ScreenRect.Width, ScreenRect.Height,
+                System.Drawing.Imaging.PixelFormat.Format32bppRgb);
+            Graphics _TempGr = Graphics.FromImage(_TempImg);
+            _TempGr.Clear(Color.Transparent);
+
+            // draw widget to temp bitmap
             if (Widget != null)
             {
-                Widget.Paint(_graphics, Rect);
+                Widget.Paint(_TempGr, Rect);
             }
             else
             {
                 Pen Pen = new Pen(Color.Gray, 1);
                 Pen.DashStyle = System.Drawing.Drawing2D.DashStyle.Dash;
-                _graphics.DrawRectangle(Pen, Rect);
-                _graphics.DrawString("Widget\nnot\nfound", new System.Drawing.Font("Verdana", 8, FontStyle.Regular),
+                _TempGr.DrawRectangle(Pen, Rect);
+                _TempGr.DrawString("Widget\nnot\nfound", new System.Drawing.Font("Verdana", 8, FontStyle.Regular),
                     new SolidBrush(Color.Yellow), Rect.X + 5, Rect.Y + 5);
             }
+
+            // write temp bitmap to buffer with semi-transparent
+            AlphaImage.DrawAlphaImage(_BufferGr, _TempImg, Rect, _Alpha);
+            //!!_BufferGr.DrawImage(_TempImg, 0, 0);
         }
 
-        public void Paint(Graphics g, bool needRepaint)
+        public void Paint(Graphics g, bool ANeedRepaint)
         {
             Region prevRegion = g.Clip;
             g.Clip = new Region(ScreenRect);
 
-            if (_needRepaint || needRepaint)
+            if (_needRepaint || ANeedRepaint)
             {
                 PaintBuffer();
                 _needRepaint = false;
             }
-            g.DrawImage(_DoubleBuffer, ScreenRect.X, ScreenRect.Y);
+
+            AlphaImage.DrawAlphaImage(g, _BufferImg, ScreenRect, 255);
+            //!!g.DrawImage(_BufferImg, ScreenRect.X, ScreenRect.Y);
             g.Clip = prevRegion;
         }
 
