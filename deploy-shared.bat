3ef11855d9625ef36248c361727da0cccf1e44c9
seregap <seregap@localhost>
2012-02-21 15:41:58 +0400

    deploy scripts

---
+++ deploy-shared.bat
@@ -0,0 +1,15 @@
+rem rmdir /S /Q D:\Работа\wm\SHARED\metrohome65
+rem mkdir D:\Работа\wm\SHARED\metrohome65
+
+mkdir D:\Работа\wm\SHARED\metrohome65\buttons
+copy D:\Работа\wm\metrohome65\metrohome65\buttons\*.* D:\Работа\wm\SHARED\metrohome65\buttons\
+mkdir D:\Работа\wm\SHARED\metrohome65\icons
+mkdir D:\Работа\wm\SHARED\metrohome65\icons\small
+copy D:\Работа\wm\metrohome65\metrohome65\icons\*.* D:\Работа\wm\SHARED\metrohome65\icons\
+copy D:\Работа\wm\metrohome65\metrohome65\icons\small\*.* D:\Работа\wm\SHARED\metrohome65\icons\small\
+
+copy D:\Работа\wm\metrohome65\metrohome65\bin\Debug\*.exe D:\Работа\wm\SHARED\metrohome65
+copy D:\Работа\wm\metrohome65\metrohome65\bin\Debug\*.dll D:\Работа\wm\SHARED\metrohome65
+copy D:\Работа\wm\metrohome65\metrohome65\Widgets\ClockWidgets\bin\Debug\*.dll D:\Работа\wm\SHARED\metrohome65
+copy D:\Работа\wm\metrohome65\metrohome65\Widgets\PhoneWidgets\bin\Debug\*.dll D:\Работа\wm\SHARED\metrohome65
+copy D:\Работа\wm\metrohome65\metrohome65\Widgets\StatusWidgets\bin\Debug\*.dll D:\Работа\wm\SHARED\metrohome65
---
+++ deploy.bat
@@ -0,0 +1,20 @@
+rem set destfolder=s:\_work\BIN
+set destfolder=D:\Work\wm\_out
+rem set srcfolder=D:\Работа\wm\metrohome65
+set srcfolder=D:\Work\wm\metrohome65
+
+rmdir /S /Q %destfolder%
+mkdir %destfolder%
+
+mkdir %destfolder%\buttons
+copy %srcfolder%\metrohome65\buttons\*.* %destfolder%\buttons\
+mkdir %destfolder%\icons
+mkdir %destfolder%\icons\small
+copy %srcfolder%\metrohome65\icons\*.* %destfolder%\icons\
+copy %srcfolder%\metrohome65\icons\small\*.* %destfolder%\icons\small\
+
+copy %srcfolder%\metrohome65\Widgets\ClockWidgets\bin\Debug\*.dll %destfolder%
+copy %srcfolder%\metrohome65\Widgets\PhoneWidgets\bin\Debug\*.dll %destfolder%
+copy %srcfolder%\metrohome65\Widgets\StatusWidgets\bin\Debug\*.dll %destfolder%
+copy %srcfolder%\metrohome65\bin\Debug\*.exe %destfolder%
+copy %srcfolder%\metrohome65\bin\Debug\*.dll %destfolder%
