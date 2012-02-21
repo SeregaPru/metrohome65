5c820f36d3956a4143d603a404316dc827dc9440
seregap <seregap@localhost>
2012-02-21 15:43:58 +0400

    1) interface for updatable widgets

---
+++ metrohome65/Widgets/BaseWidgets/IUpdatable_Intf.cs
@@ -0,0 +1,22 @@
+﻿namespace MetroHome65.Widgets
+{
+    public delegate void UpdateEventHandler();
+
+
+    public interface IActive
+    {
+        bool Active { get; set; }
+    }
+
+    /// <summary>
+    /// Additional interface for elements which may update itself
+    /// </summary>
+    public interface IUpdatable : IActive
+    {
+        /// <summary>
+        /// Event raised when Widget needs to be updated (repainted)
+        /// </summary>
+        event UpdateEventHandler OnUpdate;
+
+    }
+}
