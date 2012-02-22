3436705b0a1aa52e9f3468b98deb6678f1553d7f
seregap <seregap@localhost>
2012-02-22 09:23:07 +0400

    flatbutton

---
+++ metrohome65/HomeScreen/FlatButton.cs
@@ -0,0 +1,19 @@
+﻿using Fleux.Core;
+using Fleux.UIElements;
+
+namespace MetroHome65.HomeScreen
+{
+    public class FlatButton : TransparentImageElement
+    {
+        public FlatButton(string resourceName) : base(ResourceManager.Instance.GetIImageFromEmbeddedResource(resourceName))
+        {
+        }
+
+        public override void Draw(Fleux.Core.GraphicsHelpers.IDrawingGraphics drawingGraphics)
+        {
+            //!!base.Draw(drawingGraphics);
+            drawingGraphics.DrawText("*");
+        }
+
+    }
+}
