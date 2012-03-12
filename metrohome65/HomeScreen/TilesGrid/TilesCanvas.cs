7a995ef43a99b1af97875c58be5de3612acbbe10
SeregaPru <SeregaPru@localhost>
2012-03-13 01:01:33 +0400

    tile canvas double buffering

---
+++ metrohome65/HomeScreen/TilesGrid/TilesCanvas.cs
@@ -0,0 +1,18 @@
+﻿using Fleux.UIElements;
+
+namespace MetroHome65.HomeScreen.TilesGrid
+{
+    public class TilesCanvas : 
+        FreezedCanvas  
+        //BufferedCanvas
+    {
+
+        public void DeleteElement(UIElement element)
+        {
+            element.Parent = null;
+            element.Updated = null;
+            Children.Remove(element);
+        }
+
+    }
+}
