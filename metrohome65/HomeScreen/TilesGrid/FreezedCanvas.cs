0ca27da8060611424ac43e063178140202990976
SeregaPru <SeregaPru@localhost>
2012-03-13 01:01:33 +0400

    tile canvas double buffering

---
+++ metrohome65/HomeScreen/TilesGrid/FreezedCanvas.cs
@@ -0,0 +1,27 @@
+﻿using Fleux.UIElements;
+
+namespace MetroHome65.HomeScreen.TilesGrid
+{
+    public class FreezedCanvas : Canvas
+    {
+        #region Properties
+
+        public bool FreezeUpdate { get; set; }
+
+        #endregion
+
+        /// <summary>
+        /// Handler for update event from child.
+        /// Redraw only in non-freezed mode
+        /// </summary>
+        /// <param name="element"></param>
+        protected override void OnUpdated(UIElement element)
+        {
+            if (!FreezeUpdate)
+            {
+                this.Updated(this);
+            }
+        }
+
+    }
+}
