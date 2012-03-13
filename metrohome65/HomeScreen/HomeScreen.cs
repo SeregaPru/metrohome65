a7e54e4cabed5a3ab12d786e64cf544301cc0910
seregap <seregap@localhost>
2012-03-13 18:48:53 +0400

    try to speedup live tile animation and scrolling

--- metrohome65/HomeScreen/HomeScreen.cs
+++ metrohome65/HomeScreen/HomeScreen.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Drawing;
 using Fleux.Controls.Gestures;
 using MetroHome65.Interfaces;
--- metrohome65/HomeScreen/HomeScreenBackground.cs
+++ metrohome65/HomeScreen/HomeScreenBackground.cs
@@ -1,9 +1,7 @@
 ﻿using System;
 using System.Drawing;
-using Fleux.Controls;
 using Fleux.Styles;
 using Fleux.UIElements;
-using TinyIoC;
 
 namespace MetroHome65.HomeScreen
 {
