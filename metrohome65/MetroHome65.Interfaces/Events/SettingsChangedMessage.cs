26312bbdd63cec1089ef11a71947616539742161
SeregaPru <SeregaPru@localhost>
2012-04-01 21:52:02 +0400

    background for lockscreen

---
+++ metrohome65/MetroHome65.Interfaces/Events/SettingsChangedMessage.cs
@@ -0,0 +1,16 @@
+﻿using TinyMessenger;
+
+namespace MetroHome65.Interfaces.Events
+{
+    public class SettingsChangedMessage : ITinyMessage
+    {
+        public SettingsChangedMessage(string propertyName)
+        {
+            PropertyName = propertyName;
+        }
+
+        public readonly string PropertyName;
+
+        public object Sender { get { return null; } }
+    }
+}
