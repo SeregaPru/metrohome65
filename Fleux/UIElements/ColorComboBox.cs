aa9c4dced1943bc4ac062663a3cb94a9438795da
SeregaPru <SeregaPru@localhost>
2012-10-07 00:28:04 +0400

    font control - next step

--- Fleux/UIElements/ColorComboBox.cs
+++ Fleux/UIElements/ColorComboBox.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Drawing;
+﻿using System.Drawing;
 using Fleux.Core.GraphicsHelpers;
 using Fleux.Styles;
 
--- Fleux/UIElements/PopupEdit.cs
+++ Fleux/UIElements/PopupEdit.cs
@@ -1,14 +1,84 @@
-﻿using System.Drawing;
+﻿using System;
+using System.Drawing;
+using System.Collections.Generic;
 using Fleux.Controls;
+using Fleux.Core;
 using Fleux.Core.GraphicsHelpers;
 using Fleux.Styles;
+using Fleux.UIElements.Events;
 
 namespace Fleux.UIElements
 {
-    public class PopupPanel : Canvas
+    public class PopupPage : Canvas
     {
+        protected ApplicationBar _appBar;
+
+        public event Action OnClose;
+
+        protected internal virtual void CreateAppBar()
+        {
+            _appBar = new ApplicationBar
+            {
+                Size = new Size(Size.Width, 48 + 2 * 10),
+                Location = new Point(0, Size.Height - 48 - 2 * 10)
+            };
+            _appBar.AddButton(ResourceManager.Instance.GetBitmapFromEmbeddedResource("Fleux.Images.back.bmp"));
+            _appBar.ButtonTap += OnAppBarButtonTap;
+            AddElement(_appBar);
+        }
+
+        protected internal virtual void InitializeControls() { }
+
+        private void OnAppBarButtonTap(object sender, ButtonTapEventArgs e)
+        {
+            ParentControl.RemoveElement(this);
+            OnClose();
+        }
+
+        public override void Draw(IDrawingGraphics drawingGraphics)
+        {
+            drawingGraphics.Color(MetroTheme.PhoneBackgroundBrush);
+            drawingGraphics.FillRectangle(0, 0, Size.Width, Size.Height);
+
+            base.Draw(drawingGraphics);
+        }
+    }
 
 
+    public class FontPopupPage : PopupPage
+    {
+        private const int Padding = 20;
+        protected internal override void InitializeControls()
+        {
+            var stackPanel = new StackPanel { Size = new Size(10, 10), };
+
+            stackPanel.AddElement(new ComboBox()
+            {
+                
+                Size = new Size(Size.Width - 2 * Padding, 50),
+                Items = new List<object>() { 
+                    MetroTheme.PhoneFontFamilyNormal,
+                    MetroTheme.PhoneFontFamilyLight,
+                    MetroTheme.PhoneFontFamilySemiLight,
+                    MetroTheme.PhoneFontFamilySemiBold,
+                },
+                Style = MetroTheme.PhoneTextNormalStyle,
+            }
+                );
+            stackPanel.AddElement(new Canvas() { Size = new Size(100, 100), });
+
+            var scroller = new SolidScrollViewer
+                {
+                    Content = stackPanel,
+                    Location = new Point(Padding, Padding),
+                    Size = Size = new Size(this.Size.Width - Padding, this.Size.Height - _appBar.Size.Height - Padding),
+                    ShowScrollbars = true,
+                    HorizontalScroll = false,
+                    VerticalScroll = true,
+                };
+
+            this.AddElement(scroller);
+        }
     }
 
 
@@ -18,11 +88,13 @@ namespace Fleux.UIElements
 
         private readonly FleuxControlPage _settingsPage;
 
-        private bool _droppedDown;
+        private bool _droppedDown = false;
 
         private string _fontFamily = MetroTheme.PhoneFontFamilyNormal;
         private int _fontSize = 12;
         private Color _fontColor = MetroTheme.PhoneForegroundBrush;
+
+        private PopupPage _popupPage;
 
         #endregion
 
@@ -38,38 +110,49 @@ namespace Fleux.UIElements
         {
             _settingsPage = settingsPage;
 
-            TapHandler += p => DropDown();
+            TapHandler += p =>
+                {
+                    DropDown();
+                    return true;
+                };
         }
 
-        private void CreatePopup()
+        private PopupPage CreatePopup()
         {
-            var popupPage = new Canvas()
-                                {
-                                    Size = _settingsPage.Size,
-                                };
+            var popupPage = new FontPopupPage
+                {
+                    Size = _settingsPage.Size
+                };
+            popupPage.CreateAppBar();
+            popupPage.InitializeControls();
+            return popupPage;
         }
 
-        private bool DropDown()
+        private void DropDown()
         {
-            if (_droppedDown) return false;
+            if (_droppedDown) return;
 
-            //......
             _droppedDown = true;
 
-            //Update();
+            _popupPage = CreatePopup();
 
-            return true;
+            _popupPage.OnClose += CloseUp;
+
+            //_settingsPage.Control.AddElement(_popupPage.AnimateHorizontalEntrance(false));
+            //_settingsPage.Control.AnimateEntrance();
+
+            _settingsPage.Control.AddElement(_popupPage);
+            Update();
         }
 
-        private bool CloseUp()
+        private void CloseUp()
         {
-            if (! _droppedDown) return false;
+            if (! _droppedDown) return;
 
-            //...
             _droppedDown = false;
 
+            //_settingsPage.Control.AnimateExit();
             Update();
-            return true;
         }
 
         public override void Draw(IDrawingGraphics drawingGraphics)
