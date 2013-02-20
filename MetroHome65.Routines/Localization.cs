7f56034a5cf3194c53d02034cf4d72951b2cfccd
SeregaPru <SeregaPru@localhost>
2013-02-20 23:22:34 +0400

    localization

---
+++ MetroHome65.Routines/Localization.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+
+
+namespace MetroHome65.Routines
+{
+    public static class Extensions
+    {
+        public static string Localize(this string text)
+        {
+            return Localizer.Instance.GetText(text);
+        }
+    }
+
+
+    internal class Localizer
+    {
+        /// <summary>
+        /// two-letter language ident
+        /// </summary>
+        private string _lang;
+
+        /// <summary>
+        /// translation dictionary
+        /// key - english string
+        /// value - translated string
+        /// </summary>
+        private readonly Dictionary<string, string> _strings = new Dictionary<string, string>();
+
+        // create singleton
+        private static readonly Localizer _instance = new Localizer();
+
+        /// <summary>
+        /// Get instance of singleton.
+        /// </summary>
+        internal static Localizer Instance
+        {
+            get { return _instance; }
+        }
+
+        static Localizer()
+        { }
+
+        private Localizer()
+        {
+            _lang = CultureInfo.CurrentUICulture.TwoLetterISOLanguageName;
+
+            ReadLangStrings();
+        }
+
+        /// <summary>
+        /// Read from file translated strings for current language
+        /// </summary>
+        private void ReadLangStrings()
+        {
+            var fileName = "metrohome65.lang." + _lang;
+
+            if (!System.IO.File.Exists(fileName)) return;
+
+            using (var reader = new System.IO.StreamReader(fileName))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    if (line.StartsWith("#")) return;
+
+                    var vals = line.Trim().Split('|');
+                    if ((vals.Length == 2) && !String.IsNullOrEmpty(vals[1]))
+                        _strings.Add(vals[0], vals[1]);
+                }
+            }
+        }
+
+        internal String GetText(string name)
+        {
+            return _strings.ContainsKey(name) ? _strings[name] : name;
+        }
+    }
+}
---
+++ MetroHome65/metrohome65.lang.ru
@@ -0,0 +1,12 @@
+#плитка контакты
+Contact \n not \n found|Контакт \n не \n найден
+Contact|Контакт
+Alternate image|Альтернативный рисунок
+mobile|мобильный
+home|домашний
+work|рабочий
+text|сообщение
+SMS|СМС
+profile|профиль
+send email|отправить email
+call|позвонить на
